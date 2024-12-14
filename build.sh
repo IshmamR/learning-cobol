5b1228a0476c054423c4da3f3a785b672fe2f860
IshmamR <ishmam785@gmail.com>
2024-12-14 22:26:11 +0600

    src dir

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ file_name=$1
 
 echo "Building $file_name ..."
 
-cobc -free -x -o "./output/$file_name" "$file_name.cbl"
+cobc -free -x -o "./output/$file_name" "./src/$file_name.cbl"
 
 echo "Binary at ./output/${file_name}"
